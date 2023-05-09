ab6f2a9b58be8cf65f30197b8efafa1a28c76d88
Jakob Ingvast <co.dincrod@gmail.com>
2023-05-10 01:55:37 +0200

    Additional non-functioning functionality

--- src/Tracer.zig
+++ src/Tracer.zig
@@ -5,6 +5,7 @@ const vector = @import("utils/vector.zig");
 const FPrec = f32;
 const Vec2 = vector.Vec2T(FPrec);
 const Vec3 = vector.Vec3T(FPrec);
+const Vec4 = vector.Vec4T(FPrec);
 
 const buffer = @import("utils/buffer.zig");
 const Buffer = buffer.Buffer;
@@ -30,12 +31,16 @@ a: std.mem.Allocator,
 boxes: std.ArrayList(Box),
 colors: std.ArrayList(Vec3),
 
+camera: Camera,
+
 vao: VertexArray,
 vertex_buffer: Buffer,
 image_buffer: Buffer,
 drawer: Shader,
 
 frame: [2]u32,
+pixels: []Vec4,
+clear_color: Vec4,
 
 pub fn init(a: std.mem.Allocator) !Tracer {
     var self: Tracer = undefined;
@@ -51,9 +56,17 @@ pub fn init(a: std.mem.Allocator) !Tracer {
     self.vao.bindVertexBuffer(self.vertex_buffer, 0, 0, @sizeOf(f32) * 2);
     self.vao.setLayout(0, 2, 0, .float);
     self.frame = [2]u32{ 480, 680 };
+    self.pixels = try a.alloc(Vec4, self.frame[0] * self.frame[1]);
+    @memset(self.pixels, comptime Vec4.init(0));
     self.image_buffer = Buffer.init(@sizeOf([4]f32) * self.frame[0] * self.frame[1], .dynamic_draw);
     self.drawer = try Shader.initFile("src/shaders/image_drawer.os", a);
     self.a = a;
+    self.clear_color = Vec4.fromData(.{ 0.5, 0.5, 0, 0 });
+    self.camera = Camera.init(
+        Vec3.fromData(.{ 0, 0, 0 }),
+        Vec3.fromData(.{ 0, 0, 1 }),
+        [2]f32{ 60, 60 },
+    );
     return self;
 }
 
@@ -83,7 +96,29 @@ pub fn addBox(self: *Tracer, min: Vec3, max: Vec3, color: Vec3) void {
 }
 
 pub fn trace_scene(self: *Tracer) void {
-    _ = self;
+    const w = @intToFloat(f32, self.frame[0]);
+    const h = @intToFloat(f32, self.frame[1]);
+    for (0..self.frame[1]) |i| {
+        for (self.pixels[self.frame[0] * i .. self.frame[0] * (i + 1)], i * self.frame[0]..) |*pixel, k| {
+            const rel_pos = [2]f32{
+                @intToFloat(f32, k) / w,
+                @intToFloat(f32, i) / h,
+            };
+            const maybe_hit = self.trace(self.camera.castRay(rel_pos));
+            if (maybe_hit) |_| {
+                pixel.* = Vec4.fromData(.{ 1, 1, 1, 1 });
+            } else {
+                pixel.* = self.clear_color;
+            }
+        }
+    }
+    self.image_buffer.subData(
+        0,
+        @sizeOf(Vec4) * self.frame[0] * self.frame[1],
+        self.pixels,
+    ) catch |err| {
+        log.err("Failed to upload image to GPU buffer! Error: {}", .{err});
+    };
 }
 
 pub fn draw(self: *Tracer) void {
@@ -110,9 +145,27 @@ pub fn trace(self: Tracer, ray: Ray) ?Hit {
             } else {
                 hit_pos = pos.add(ray.direction.sMult(tmin));
             }
-            break :hit_blk Hit{ .position = hit_pos, .hit_index = i };
+            break :hit_blk Hit{ .position = hit_pos, .hit_index = @intCast(u32, i) };
         }
     } else {
         break :hit_blk null;
     };
+}
+
+test "Tracing" {
+    const a = std.testing.allocator;
+    var tracer = try Tracer.init(a);
+    tracer.addCube(Vec3.fromData(.{
+        0,
+        0,
+        1,
+    }));
+    std.testing.expect(tracer.trace(Ray{
+        .origin = Vec3{ .x = 0, .y = 0, .z = 0 },
+        .direction = Vec3{ .x = 0, .y = 0, .z = 1 },
+    }) != null);
+    std.testing.expect(tracer.trace(Ray{
+        .origin = Vec3{ .x = 0, .y = 0, .z = 0 },
+        .direction = Vec3{ .x = 0, .y = 1, .z = 0 },
+    }) == null);
 }
